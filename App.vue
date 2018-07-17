275d9600379aa05f2913c9983cc9bea1ca4cf4ee
mlikoga <marcelolikoga@gmail.com>
2018-07-16 22:23:07 -0300

    lock files

--- App.vue
+++ App.vue
@@ -1,17 +1,17 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">My Vue Native App</text>
+    <text class="text-color-primary">Bolão F1 app</text>
     </view>
 </template>
  
 <style>
 .container {
-  background-color: white;
+  background-color: darkslategray;
   align-items: center;
   justify-content: center;
   flex: 1;
 }
 .text-color-primary {
-  color: blue;
+  color: red;
 }
 </style>
