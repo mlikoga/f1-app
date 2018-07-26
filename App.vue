441d6f691f4d6b190674c725be750befd4a5f2e4
mlikoga <marcelolikoga@gmail.com>
2018-07-26 20:00:13 -0300

    Added NativeBase components

--- App.vue
+++ App.vue
@@ -3,14 +3,21 @@
 </template>
  
 <script>
+import Vue from "vue-native-core";
+import { VueNativeBase } from "native-base";
 import { StackNavigator } from "vue-native-router";
 import LoginScreen from "./src/screens/LoginScreen";
 import MainTabsScreen from "./src/screens/MainTabsScreen";
+import ApostaScreen from "./src/screens/ApostaScreen";
+
+// registering all native-base components to the global scope of the Vue
+Vue.use(VueNativeBase);
 
 const AppNavigation = StackNavigator(
   {
     Login: LoginScreen,
     Main:  MainTabsScreen,
+    Aposta: ApostaScreen,
   },
   {
     initialRouteName: 'Login',
